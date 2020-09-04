2799a96a31cb0d17ec063e1a05237a6636baf6e7
Daniel Fleck <danielfleck@live.ca>
2020-09-04 10:53:24 -0300

    Improve css and fixed nav bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,23 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  menuItems = [
+    {linkId: 1, linkName: 'ônibus'},
+    {linkId: 2, linkName: 'Lotação'}
+  ]
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  display(linkId) {
+    console.log(linkId);
+    if(linkId == 1) {
+      this.router.navigate(['linhas-onibus']);  
+    }
+    if(linkId == 2) {
+      this.router.navigate(['linhas-lotacao']);
+    }
   }
 
 }
